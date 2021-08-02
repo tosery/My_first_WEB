7720a7e91c6df9b357d4c12e3cf6de68d0503660
tosery <83147935+tosery@users.noreply.github.com>
2021-08-02 14:23:11 +0900

    add layoutpractice
    ==

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
 alert("공란으로 로그인 가능합니다^^")
-const a = 5;
-
-console.log(a/2);
-console.log(a+2);
-console.log(a*2);
-// variable을 사용해보자
-
---
+++ memo.js
@@ -0,0 +1,19 @@
+// let a = 5;
+// let myName = "juny"
+
+// 자바스크립트는 const와 let 두가지 방법으로 변수선언
+// const는 고정 let은 바꿀 수 있음
+// javascript는 오래된 방식도 상존한다.
+//예전에는 var이라는 선언을 했다. 그런데 이 var는 보호를 받지 못한다.
+
+// console.log(a/2);
+// console.log(a+2);
+// console.log(a*2);
+// console.log("hello" + myName);
+
+// variable을 사용해보자
+// let a = null; 이런식
+// let a; 이거는 undefined
+//null은 true나 false가 아니다. 
+// undefined 와 null은 하나의 타입으로 보는게 맞다.
+//다큐먼트 객체 write메서드
